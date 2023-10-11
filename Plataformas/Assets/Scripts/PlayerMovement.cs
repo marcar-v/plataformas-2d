23f353ddbcb44c8a24133cc36f011ba74737f1fe
marcar-v <vmarcar95@gmail.com>
2023-10-11 14:19:29 +0200

    Integrado doble salto

--- Plataformas/Assets/Scripts/PlayerMovement.cs
+++ Plataformas/Assets/Scripts/PlayerMovement.cs
@@ -4,53 +4,69 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    [Header("Movement")]
     [SerializeField] float runSpeed;
-    [SerializeField] float jumpForce = 3f;
     private Rigidbody2D rb;
     [SerializeField] float speed = 1.5f;
 
+    [Header("Jump")]
+    [SerializeField] float jumpForce = 3f;
     [SerializeField] BoxCollider2D boxCollider;
     [SerializeField] LayerMask groundLayer;
-    [SerializeField] Transform groundCheck;
-    [SerializeField] LayerMask groundLayer;
-    bool isGrounded;
 
-    [SerializeField] bool largeJump = false;
-    [SerializeField] float fallMultiplier = 0.5f;
-    [SerializeField] float lowJumpMultiplier = 1f;
+    [Header("Double Jump")]
+    [SerializeField] int totalJumps = 1;
+    private int remainingJumps;
 
     [SerializeField] float doubleJumpForce = 2.5f;
     private bool canDoubleJump;
 
+    [Header("Large Jump")]
+    [SerializeField] bool largeJump = false;
+    [SerializeField] float fallMultiplier = 0.5f;
+    [SerializeField] float lowJumpMultiplier = 1f;
+
+    [Header("Animations")]
     [SerializeField] SpriteRenderer sprite;
     [SerializeField] Animator animator;
+
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        remainingJumps = totalJumps;
     }
 
     void Update()
     {
         Movement();
         Jump();
-        isGrounded = Physics2D.OverlapBox(groundCheck.position, new Vector2(0.1f, 0.006f), 0, groundLayer);
     }
-
 
     bool isGrounded()
     {
-        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.2f, groundLayer);
-        return raycastHit.collider != null;
+        RaycastHit2D rayCastHit = Physics2D.BoxCast(boxCollider.bounds.center, new Vector2(boxCollider.bounds.size.x, boxCollider.bounds.size.y), 0f, Vector2.down, 0.2f, groundLayer);
+        return rayCastHit.collider != null;
     }
 
     void Jump()
     {
-        if(Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        //Controlador salto y doble salto
+        if(isGrounded())
         {
+            remainingJumps = totalJumps;
+        }
+        if(Input.GetKeyDown(KeyCode.Space) && remainingJumps > 0)
+        {
+            animator.SetBool("DoubleJump", true);
+            remainingJumps--;
+            rb.velocity = new Vector2(rb.velocity.x, 0f);
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
         }
 
+
+        //Condicionales para controlar las animaciones
         if (isGrounded() == false)
         {
             animator.SetBool("Jump", true);
@@ -60,9 +76,11 @@ public class PlayerMovement : MonoBehaviour
         if (isGrounded())
         {
             animator.SetBool("Jump", false);
+            animator.SetBool("DoubleJump", false);
             animator.SetBool("Falling", false);
         }
 
+        //Animación y controlador de caída
         if (rb.velocity.y < 0)
         {
             animator.SetBool("Falling", true);
@@ -72,7 +90,7 @@ public class PlayerMovement : MonoBehaviour
             animator.SetBool("Falling", false);
         }
 
-
+        //Salto mejorado. Cuanto más se pulse la tecla "Espacio" más alto hace el salto
         if (largeJump)
         {
             if (rb.velocity.y < 0)
