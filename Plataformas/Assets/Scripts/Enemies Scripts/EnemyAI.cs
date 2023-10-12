33f53c292b772a8cde56a8ea86c5bdcc434a203b
marcar-v <vmarcar95@gmail.com>
2023-10-12 11:10:30 +0200

    Basic Enemy AI

--- Plataformas/Assets/Scripts/Enemies Scripts/EnemyAI.cs
+++ Plataformas/Assets/Scripts/Enemies Scripts/EnemyAI.cs
@@ -1,18 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Graphics")]
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] float speed = 0.5f;
+
+    [Header("Time")]
+    [SerializeField] float startWaitTime = 2f;
+    private float waitTime;
+
+
+    [Header("Move Spots")]
+    [SerializeField] Transform[] moveSpots; 
+    
+    private int i = 0;
+
+    private Vector2 actualPos;
+
+
+
+
     void Start()
     {
-        
+        waitTime = startWaitTime; 
     }
 
-    // Update is called once per frame
     void Update()
     {
+        StartCoroutine(CheckEnemyMovement());
+
+        transform.position = Vector2.MoveTowards(transform.position, moveSpots[i].transform.position, speed * Time.deltaTime);
         
+        if (Vector2.Distance(transform.position, moveSpots[i].transform.position) < 0.1f)
+        {
+            if (waitTime <= 0)
+            {
+                if (moveSpots[i] != moveSpots[moveSpots.Length - 1])
+                {
+                    i++;
+                }
+                else
+                {
+                    i = 0;
+                }
+                waitTime = startWaitTime;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
+    }
+
+    IEnumerator CheckEnemyMovement()
+    {
+        actualPos = transform.position;
+        yield return new WaitForSeconds(0.5f);
+
+        if( transform.position.x > actualPos.x)
+        {
+            spriteRenderer.flipX = true;
+        }
+        else if (transform.position.x < actualPos.x)
+        {
+            spriteRenderer.flipX = false;
+        }
+
     }
 }
