d4305205ea250ac8469471b0afbcaa238f7bb155
marcar-v <vmarcar95@gmail.com>
2023-10-13 12:32:08 +0200

    Added basic box

---
+++ Plataformas/Assets/Scripts/Enviroment Scripts/JumpBox.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JumpBox : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+    [SerializeField] SpriteRenderer spriteRenderer;
+    [SerializeField] GameObject brokenParts;
+    [SerializeField] float jumpForce = 4f;
+    [SerializeField] int boxLifes = 1;
+    [SerializeField] GameObject boxCollider;
+    [SerializeField] Collider2D col2D;
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.transform.CompareTag("Player"))
+        {
+            collision.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;
+            LoseLifeAndHit();
+        }
+    }
+
+
+    public void LoseLifeAndHit()
+    {
+        boxLifes--;
+        animator.Play("Hit");
+        CheckBoxLife();
+    }
+
+    public void CheckBoxLife()
+    {
+        if(boxLifes <= 0)
+        {
+            boxCollider.SetActive(false);
+            col2D.enabled = false;
+            brokenParts.SetActive(true);
+            spriteRenderer.enabled = false;
+            Invoke("DestroyBox", 0.5f);
+        }
+    }
+
+    public void DestroyBox()
+    {
+        Destroy(gameObject);
+    }
+
+}
